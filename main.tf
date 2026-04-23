d34bfa982bd31f33fd36f60de17b38a92a43ae27
Wallace <wallacewfb@gmail.com>
2026-04-23 08:30:24 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,41 +11,31 @@ provider "aws" {
   region = var.aws_region
 }
 
-# VPC Profissional
+# Criando a VPC
 resource "aws_vpc" "main" {
   cidr_block           = var.vpc_cidr
   enable_dns_hostnames = true
   tags = { Name = "${var.project_name}-VPC" }
 }
 
-# Subnets Públicas (Para o Load Balancer)
+# Subnet Pública 1
 resource "aws_subnet" "public_1" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.1.0/24"
   availability_zone = "${var.aws_region}a"
-  map_public_ip_on_launch = true
   tags = { Name = "${var.project_name}-Public-1a" }
 }
 
+# Subnet Pública 2 (Alta Disponibilidade)
 resource "aws_subnet" "public_2" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.2.0/24"
   availability_zone = "${var.aws_region}b"
-  map_public_ip_on_launch = true
   tags = { Name = "${var.project_name}-Public-1b" }
 }
 
-# Internet Gateway para saída de rede
-resource "aws_internet_gateway" "gw" {
+# Internet Gateway
+resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
   tags   = { Name = "${var.project_name}-IGW" }
-}
-
-# Application Load Balancer (ALB)
-resource "aws_lb" "web_alb" {
-  name               = "${var.project_name}-ALB"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.alb_sg.id]
-  subnets            = [aws_subnet.public_1.id, aws_subnet.public_2.id]
 }
